7588e70f0920dcde4bbda519c578155bc05be063
susmi-cse <162762272+susmi-cse@users.noreply.github.com>
2024-11-17 02:53:05 +0530

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,14 +8,10 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone, I am <span className="purple">SUSMITA SARKAR</span>
+            from <span className="purple"> Wwst Bengal, India.</span>
             <br />
-            I am currently employed as a software developer at Juspay.
-            <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
-            <br />
+            I am currently pursuing BTech in Computer Science Engineering at Indian Institute of Information Technology, Agartala.
             <br />
             Apart from coding, some other activities that I love to do!
           </p>
@@ -24,7 +20,7 @@ function AboutCard() {
               <ImPointRight /> Playing Games
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
+              <ImPointRight /> Drawing potraits
             </li>
             <li className="about-activity">
               <ImPointRight /> Travelling
@@ -34,7 +30,7 @@ function AboutCard() {
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Susmita</footer>
         </blockquote>
       </Card.Body>
     </Card>
